799fa8b288a6872c1df0f6b0e386b6cf335779b0
oakes <zsoakes@gmail.com>
2016-03-01 10:40:26 -0500

    Use database

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -2,10 +2,12 @@
  * Created by zach on 2/8/16.
  */
 public class ToDoItem {
+    public int id;
     public String text;
     public boolean isDone;
 
-    public ToDoItem(String text, boolean isDone) {
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
